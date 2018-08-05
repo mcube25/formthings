e3a1ce07c63e8db61322ac638d298936f76cbe2f
Muhammad Muhktar Musa <mcube3567@gmail,com>
2018-08-05 23:47:26 +0100

    multiple arrays

---
+++ arrarray.php
@@ -0,0 +1,10 @@
+<?php
+//two dimensional array
+$cars = array(
+    array('volvo',22,18),
+    array('bmw',15,13),
+    array('saab',5,2),
+    array('land rover',17,15)
+);
+echo $cars[0][0].': in stock'.$cars[0][1].':sold'.$cars[0][2].'<br>'
+?>
