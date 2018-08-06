11c1df70d915444ff48ea5e5f715f88f37d69a7c
Muhammad Muhktar Musa <mcube3567@gmail,com>
2018-08-06 23:08:05 +0100

    for loop

--- arrarray.php
+++ arrarray.php
@@ -11,3 +11,13 @@ echo $cars[1][0].': in stock'.$cars[1][1].':sold'.$cars[1][2].'<br>';
 echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
 echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
 ?>
+<?php
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+    echo "<li>".$cars[$row][$col]."</li>";
+  }
+  echo "</ul>";
+}
+?>
