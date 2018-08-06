252aa972d593e915bcae44163604ae175683c84c
Muhammad Muhktar Musa <mcube3567@gmail,com>
2018-08-06 22:54:35 +0100

    arays within

--- arrarray.php
+++ arrarray.php
@@ -6,5 +6,8 @@ $cars = array(
     array('saab',5,2),
     array('land rover',17,15)
 );
-echo $cars[0][0].': in stock'.$cars[0][1].':sold'.$cars[0][2].'<br>'
+echo $cars[0][0].': in stock'.$cars[0][1].':sold'.$cars[0][2].'<br>';
+echo $cars[1][0].': in stock'.$cars[1][1].':sold'.$cars[1][2].'<br>';
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
 ?>
