bfa2dacbc5035e66b18b44742da341154e871090
Muhammad Muhktar Musa <mcube3567@gmail,com>
2018-08-09 00:07:37 +0100

    date and time

---
+++ date.php
@@ -0,0 +1,14 @@
+<?php
+date_default_timezone_set("America/New_York");
+echo "The time is " . date("h:i:sa");
+
+//h-12 hour format with a leading o 
+//i-mitutes with leading zeros 00-59
+//s-seconds with leading zeros
+//a- lowercase anti meridien and post meridian
+//php date()function will return the current date/time of the server
+?>
+<?php
+date_default_timezone_set("America/New_York");
+echo "The time is " . date("h:i:sa");
+?>
